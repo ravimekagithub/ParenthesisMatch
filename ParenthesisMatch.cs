0928b6f5fb63e0323e2c52ae8ec7b04bfaed36d6
ravimekagithub <34259801+ravimekagithub@users.noreply.github.com>
2017-12-04 23:19:51 -0500

    Initial Commit
    Initial Commit

---
+++ ParenthesisMatch.cs
@@ -0,0 +1,29 @@
+﻿namespace ParenthesisMatch
+{
+	public class ParenthesisMatch
+	{
+		public char FirstValuetoCompare { get; set; }
+		public char SecondValuetoCompare { get; set; }
+
+
+		public bool CompareifParenthesisareMatching(string inputValue)
+		{
+			int opencount = 0, closecount = 0;
+
+			for (var i = 0; i < inputValue.Length; i++)
+			{
+				if (inputValue[i] == FirstValuetoCompare)
+					opencount++;
+
+				if (inputValue[i] == SecondValuetoCompare)
+					closecount++;                                                                                                                                                                                                                                                                                     
+			}
+
+			if (opencount == closecount) return true;
+			                                               
+			return false;
+		}
+
+		
+	}
+}
